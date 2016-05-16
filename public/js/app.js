8226bf9865b700302e29634fcb2d405d384e2b01
garciamax <garciamax@gmail.com>
2016-05-16 09:22:48 +0300

    bug fixed

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,38 @@
 (function () {
     var App = function () {
         var _ = {
-            lastMsgId:0,
+            lastMsgId:0,mutex:false,
             $nickname:{},$message:{},$chatbox:{},
-            hangleMessages: function hangleMessages(response){
+            handleMessages: function hangleMessages(response){
+                if(_.mutex) return;
+                _.mutex = true;
                 if($.isArray(response)){
                     response.map(_.drawMessage);
                     var _lastmessage = response[response.length-1];
                     if(_lastmessage) {
-                        l('a',response);
-                        _.getMessages(_lastmessage.messages_id);
                         _.lastMsgId = _lastmessage.messages_id;
-                    }else{
-                        l('b',_.lastMsgId,response);
-                        _.getMessages(_.lastMsgId);
                     }
                 }else{
                     _.drawMessage(response);
-                    _.getMessages(response.messages_id);
                     _.lastMsgId = response.messages_id;
                 }
+                setTimeout(function () {
+                    _.mutex = false;
+                    _.getMessages(_.lastMsgId);
+                },1000)
             },
             drawMessage:function drawMessage(msgObj){
-                var _msg = (msgObj.nickname||msgObj.user_id) + ': ' + msgObj.message + '</br>';
+                var _msg = (msgObj.nickname||msgObj.user_id) + ': ' + msgObj.message + '<br>';
                 _.$chatbox.append(_msg)
             },
             getMessages: function getMessages(lastId){
-                var _timeout = 50;
-                if(lastId)_timeout=1000;
-                setTimeout(function(){
-                    $.getJSON('/api/messages' + (lastId ? '?last_id='+lastId : ''),_.hangleMessages)
-                },_timeout);
+               $.getJSON('/api/messages' + (lastId ? '?last_id='+lastId : ''),_.handleMessages)
             },
             sendMessage: function sendMessage(e){
                 e.preventDefault();
                 $.post('/api/message',
                     {nickname:_.$nickname.val(),message:_.$message.val()},
-                    _.hangleMessages)
+                    _.handleMessages)
             },
             bind:function bind(){
                 $('form').on('submit',_.sendMessage);
